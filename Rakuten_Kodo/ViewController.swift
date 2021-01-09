a5d51fc43b594998cf9ef0f0cff6bd15749fba02
John Fowler <jcodyfowler@gmail.com>
2021-01-10 08:35:54 +0900

    Initial Commit

---
+++ Rakuten_Kodo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rakuten_Kodo
+//
+//  Created by John Fowler on 1/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
